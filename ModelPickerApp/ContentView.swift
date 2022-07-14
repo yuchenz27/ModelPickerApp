34902b1af67c0d149b110b4eea0be2ab4f0db331
yuchen <yuchen@holoi.com>
2022-07-14 17:18:02 +0800

    Updated

--- ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
+    
+    private var models: [String] = {
+       // Dynamically get model filenames
+        let fileManager = FileManager.default
+        
+        guard let path = Bundle.main.resourcePath, let files = try? fileManager.contentsOfDirectory(atPath: path) else {
+            return []
+        }
+        
+        var availableModels: [String] = []
+        for filename in files where filename.hasSuffix("usdz") {
+            let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+            availableModels.append(modelName)
+        }
+        return availableModels
+    }()
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            
+            if self.isPlacementEnabled {
+                PlacementButtonsView()
+            } else {
+                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, models: self.models)
+            }
+        }
     }
 }
 
@@ -20,18 +46,70 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    @Binding var isPlacementEnabled: Bool
+    
+    var models: [String]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< self.models.count) { index in
+                    Button(action: {
+                        print("DEBUG: selected model with name \(self.models[index])")
+                        
+                        self.isPlacementEnabled = true
+                    }) {
+                        Image(uiImage: UIImage(named: self.models[index])!)
+                            .resizable()
+                            .frame(height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.white)
+                            .cornerRadius(12)
+                    }.buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding(20)
+        .background(Color.black.opacity(0.5))
+    }
+}
+
+struct PlacementButtonsView: View {
+    var body: some View {
+        HStack {
+            // Cancel button
+            Button(action: {
+                print("DEBUG: Model placement cancelled")
+            }) {
+                Image(systemName: "xmark")
+                    .frame(width:60, height: 60)
+                    .font(.title)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
+            // Confirm button
+            Button(action: {
+                print("DEBUG: Model placement confirmed")
+            }) {
+                Image(systemName: "checkmark")
+                    .frame(width:60, height: 60)
+                    .font(.title)
+                    .background(Color.white.opacity(0.75))
+                    .cornerRadius(30)
+                    .padding(20)
+            }
+        }
+    }
 }
 
 #if DEBUG
